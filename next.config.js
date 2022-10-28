0c4507da1fe87206d6435e7006376d7af48da8c1
Marco Calderon <mcalderon.code@gmail.com>
2022-10-28 14:07:44 -0700

    Updated config and added gitlab ci configuration file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	assetPrefix:
+		process.env.NODE_ENV === 'production' ? '/marco-calderon.gitlab.io' : '',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
