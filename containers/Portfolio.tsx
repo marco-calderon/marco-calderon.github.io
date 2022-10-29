032ad82dd22ff1279cb40ae93d5516d06412d598
Marco Calderon <mcalderon.code@gmail.com>
2022-10-29 11:23:26 -0700

    Added motion

--- containers/Portfolio.tsx
+++ containers/Portfolio.tsx
@@ -1,13 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { PortfolioModel } from '../lib/models/portfolio.model';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export type PortfolioProps = {
 	projects: PortfolioModel[];
 };
 
+export type Filter = 'web' | 'mobile' | 'full' | 'all';
+
 const Portfolio = ({ projects }: PortfolioProps) => {
+	const [filter, setFilter] = useState<Filter>('all');
+
+	const filteredProjects = useMemo(
+		() =>
+			projects.filter((p) => (filter === 'all' ? true : p.category === filter)),
+		[projects, filter]
+	);
+
 	return (
 		<section id="portfolio" className="portfolio">
 			<div className="container" data-aos="fade-up">
@@ -19,12 +30,30 @@ const Portfolio = ({ projects }: PortfolioProps) => {
 				<div className="row" data-aos="fade-up" data-aos-delay="100">
 					<div className="col-lg-12 d-flex justify-content-center">
 						<ul id="portfolio-flters">
-							<li data-filter="*" className="filter-active">
+							<li
+								className={filter === 'all' ? 'filter-active' : ''}
+								onClick={() => setFilter('all')}
+							>
 								All
 							</li>
-							<li data-filter=".filter-full">Web App</li>
-							<li data-filter=".filter-mobile">Mobile</li>
-							<li data-filter=".filter-web">Front end</li>
+							<li
+								className={filter === 'full' ? 'filter-active' : ''}
+								onClick={() => setFilter('full')}
+							>
+								Web App
+							</li>
+							<li
+								className={filter === 'mobile' ? 'filter-active' : ''}
+								onClick={() => setFilter('mobile')}
+							>
+								Mobile
+							</li>
+							<li
+								className={filter === 'web' ? 'filter-active' : ''}
+								onClick={() => setFilter('web')}
+							>
+								Front end
+							</li>
 						</ul>
 					</div>
 				</div>
@@ -33,36 +62,44 @@ const Portfolio = ({ projects }: PortfolioProps) => {
 					data-aos="fade-up"
 					data-aos-delay="200"
 				>
-					{projects &&
-						projects.map((p) => (
-							<div
-								key={p.id}
-								className={`col-lg-4 col-md-6 portfolio-item filter-${p.category}`}
-							>
-								<Link href={`/projects/${p.id}`}>
-									<div className="portfolio-wrap">
-										<Image
-											src={p.imgUrl}
-											className="img-fluid"
-											alt=""
-											width={p.width}
-											height={p.height}
-										/>
-										<div className="portfolio-info">
-											<h4>{p.title}</h4>
-											<div className="mb-2 d-flex flex-row gap-1">
-												{p.tags &&
-													p.tags.map((t) => (
-														<span key={t.id} className={`badge bg-${t.class}`}>
-															{t.name}
-														</span>
-													))}
+					<AnimatePresence>
+						{filteredProjects &&
+							filteredProjects.map((p) => (
+								<motion.div
+									key={p.id}
+									initial={{ opacity: 0, height: 0 }}
+									animate={{ opacity: 1, height: 'auto' }}
+									exit={{ opacity: 0, height: 0 }}
+									className={`col-lg-4 col-md-6 portfolio-item filter-${p.category}`}
+								>
+									<Link href={`/projects/${p.id}`}>
+										<div className="portfolio-wrap">
+											<Image
+												src={p.imgUrl}
+												className="img-fluid"
+												alt=""
+												width={p.width}
+												height={p.height}
+											/>
+											<div className="portfolio-info">
+												<h4>{p.title}</h4>
+												<div className="mb-2 d-flex flex-row gap-1">
+													{p.tags &&
+														p.tags.map((t) => (
+															<span
+																key={t.id}
+																className={`badge bg-${t.class}`}
+															>
+																{t.name}
+															</span>
+														))}
+												</div>
 											</div>
 										</div>
-									</div>
-								</Link>
-							</div>
-						))}
+									</Link>
+								</motion.div>
+							))}
+					</AnimatePresence>
 				</div>
 			</div>
 		</section>
