f3b0163b1ff2b6ea2c3f9ecc9467b6a947e6a896
Antonio Calderon <mcalderon.code@gmail.com>
2022-10-28 20:24:35 -0700

    Removed unused config and functions from Next, adjusted scriptsquare image

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-	assetPrefix:
-		process.env.NODE_ENV === 'production' ? '/marco-calderon.gitlab.io' : '',
-	images: {
-		unoptimized: true
-	}
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
