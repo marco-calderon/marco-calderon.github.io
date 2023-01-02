05fb9a0d06ed000dd0109b9f855706a99a5b8f8e
Marco Calderon <mcalderon.code@gmail.com>
2023-01-01 17:10:44 -0800

    Styling for full screen menu on dark and light modes

--- containers/FullScreenMenu.tsx
+++ containers/FullScreenMenu.tsx
@@ -29,7 +29,7 @@ const FullScreenMenu = ({ open, onClose }: FullScreenMenuProps) => {
 		<AnimatePresence>
 			{open && (
 				<motion.div
-					className="z-[60] flex fixed top-0 bottom-0 left-0 right-0 bg-white/60 w-full h-[100vh] items-center justify-center backdrop-blur-lg"
+					className="z-[60] flex fixed top-0 bottom-0 left-0 right-0 bg-white/60 dark:bg-gray-500/60 w-full h-[100vh] items-center justify-center backdrop-blur-lg"
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
 					exit={{ opacity: 0 }}
@@ -40,32 +40,50 @@ const FullScreenMenu = ({ open, onClose }: FullScreenMenuProps) => {
 						animate="show"
 						className="flex flex-col items-center gap-4 text-center transition-all"
 					>
-						<Link href="/#about" className="text-4xl font-bold scrollto">
+						<Link
+							href="/#about"
+							className="text-4xl font-bold text-blue-500 transition-all scrollto dark:text-white hover:text-blue-700 dark:hover:text-gray-200"
+						>
 							<motion.h4 variants={item} onClick={onClose} style={linkStyle}>
 								About
 							</motion.h4>
 						</Link>
-						<Link href="/#services" className="text-4xl font-bold scrollto">
+						<Link
+							href="/#services"
+							className="text-4xl font-bold text-blue-500 transition-all scrollto dark:text-white hover:text-blue-700 dark:hover:text-gray-200"
+						>
 							<motion.h4 variants={item} onClick={onClose} style={linkStyle}>
 								Services
 							</motion.h4>
 						</Link>
-						<Link href="/#skills" className="text-4xl font-bold scrollto">
+						<Link
+							href="/#skills"
+							className="text-4xl font-bold text-blue-500 transition-all scrollto dark:text-white hover:text-blue-700 dark:hover:text-gray-200"
+						>
 							<motion.h4 variants={item} onClick={onClose} style={linkStyle}>
 								Skills
 							</motion.h4>
 						</Link>
-						<Link href="/#portfolio" className="text-4xl font-bold scrollto">
+						<Link
+							href="/#portfolio"
+							className="text-4xl font-bold text-blue-500 transition-all scrollto dark:text-white hover:text-blue-700 dark:hover:text-gray-200"
+						>
 							<motion.h4 variants={item} onClick={onClose} style={linkStyle}>
 								Portfolio
 							</motion.h4>
 						</Link>
-						<Link href="/#contact" className="text-4xl font-bold scrollto">
+						<Link
+							href="/#contact"
+							className="text-4xl font-bold text-blue-500 transition-all scrollto dark:text-white hover:text-blue-700 dark:hover:text-gray-200"
+						>
 							<motion.h4 variants={item} onClick={onClose} style={linkStyle}>
 								Contact
 							</motion.h4>
 						</Link>
-						<Link href="/resume" className="text-4xl font-bold scrollto">
+						<Link
+							href="/resume"
+							className="text-4xl font-bold text-blue-500 transition-all scrollto dark:text-white hover:text-blue-700 dark:hover:text-gray-200"
+						>
 							<motion.h4 variants={item} onClick={onClose} style={linkStyle}>
 								Resume
 							</motion.h4>
