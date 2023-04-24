62ab8848563d5a28c1ef767147fd30c41cf28cef
mcalderon.code@gmail.com <Antonio Calderon>
2023-04-23 18:28:22 -0700

    chore: :heavy_plus_sign: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
 import '../styles/globals.css';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -14,7 +15,13 @@ function MyApp({ Component, pageProps }: AppProps) {
     });
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
