c5a8b5bc4ebd44981f21d787e7da02f418a8802f
mcalderon.code@gmail.com <Antonio Calderon>
2023-05-05 16:00:39 -0700

    feat: :art: grid layout fixes for large screens

--- containers/Products.tsx
+++ containers/Products.tsx
@@ -9,7 +9,7 @@ export type ProductsProps = {
 
 const Products = ({ products }: ProductsProps) => {
   return (
-    <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-2">
+    <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
       {products.map((p) => (
         <Link
           href={p.storeUrl}
